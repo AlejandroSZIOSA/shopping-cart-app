7425d36c61d7d42eb161b28b981438c9eff05731
AlejandroSZIOSA <ggato33@gmail.com>
2025-12-30 17:54:56 +0100

    add check-coverage lib, configure json.script, set envoriment variables

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,18 @@
-import { useEffect, useState, type FC } from "react";
+import { useState, type FC } from "react";
 import { type Product } from "../../types/shared";
 import { Header } from "../../components/Header/Header";
 import { useCartContext } from "../../components/hooks/useCartContext";
 import { CartShowing } from "../../components/CartShowing/CartShowing";
 import { List } from "../../components/List/List";
+import { VITE_BASE_URL } from "../../utils/constants";
 
 const initialProducts: Product[] = [
   { id: 1, name: "Product 1", price: 10, qty: 0, item_total: 10 },
   { id: 2, name: "Product 2", price: 20, qty: 0, item_total: 20 },
   { id: 3, name: "Product 3", price: 30, qty: 0, item_total: 30 },
 ];
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const HomePage: FC = () => {
   const [products, setProducts] = useState<Product[]>(initialProducts);
@@ -18,9 +21,8 @@ export const HomePage: FC = () => {
 
   const [openCart, setOpenCart] = useState(false);
 
-  const [isDisabled, setIsDisabled] = useState(false);
-
-  useEffect(() => {}, []);
+  console.log(API_KEY);
+  console.log(VITE_BASE_URL);
 
   const handleAddToCart = (product: Product) => {
     product.qty = 1;
